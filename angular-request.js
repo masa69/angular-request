69b8f0b751c5196ef71dabc013d416d8bc2e201e
masa69 <psychedelic.nekopunch@gmail.com>
2016-10-02 01:26:36 +0900

    request -> httpRequest

--- angular-request.js
+++ angular-request.js
@@ -1,57 +1,63 @@
 angular.module('angular-request', [])
-.service('$request', ['$http', '$httpParamSerializerJQLike', function($http, $httpParamSerializerJQLike) {
+.service('$httpRequest', ['$http', '$httpParamSerializerJQLike', function($http, $httpParamSerializerJQLike) {
 
 	'use strict';
 
 	var self = {};
 
-	var apiUrl = '';
-
-	self.setUrl = function(url) {
-		apiUrl = url;
+	self.config = {
+		get: {
+			method          : 'GET',
+			url             : null,
+			params          : null,
+			// cache           : $templateCache,
+			paramSerializer : '$httpParamSerializerJQLike',
+			headers         : { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+		},
+		post: {
+			method           : 'POST',
+			url              : null,
+			data             : null,
+			headers          : { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
+			withCredentials  : true,
+			transformRequest : angular.identity,
+		},
+		upload: {
+			method           : 'POST',
+			url              : null,
+			data             : null,
+			headers          : { 'Content-Type': undefined },
+			withCredentials  : true,
+			transformRequest : null,
+		},
 	};
 
 	self.get = function(url, params, callback) {
 
-		var url = apiUrl + url;
 		var sendParams = (params) ? params : {};
 
-		$http({
-			method          : 'GET',
-			url             : url,
-			params          : sendParams,
-			// cache           : $templateCache,
-			paramSerializer : '$httpParamSerializerJQLike',
-			headers         : { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' }
-		}).then(function(response) {
-				callback(false, response, response.data);
-			}, function(response) {
-				callback(true, response, response.data);
-			});
+		self.config.get.url    = url;
+		self.config.get.params = sendParams;
+
+		send(self.config.get, function(error, httpResponse, body) {
+			if (callback) callback(error, httpResponse, body);
+		});
 	};
 
 	self.post = function(url, params, callback) {
 
-		var url = apiUrl + url;
 		var sendParams = (params) ? params : {};
 
-		$http({
-			method           : 'POST',
-			url              : url,
-			data             : $httpParamSerializerJQLike(sendParams),
-			headers          : { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' },
-			withCredentials  : true,
-			transformRequest : angular.identity
-		}).then(function(response) {
-				callback(false, response, response.data);
-			}, function(response) {
-				callback(true, response, response.data);
-			});
+		self.config.post.url  = url;
+		self.config.post.data = $httpParamSerializerJQLike(sendParams);
+
+		send(self.config.post, function(error, httpResponse, body) {
+			if (callback) callback(error, httpResponse, body);
+		});
 	};
 
 	self.upload = function(url, params, callback) {
 
-		var url = apiUrl + url;
 		var sendParams = new FormData();
 
 		angular.forEach(params, function(param, key) {
@@ -60,18 +66,19 @@ angular.module('angular-request', [])
 			}
 		});
 
-		$http({
-			method           : 'POST',
-			url              : url,
-			data             : sendParams,
-			headers          : { 'Content-Type': undefined },
-			withCredentials  : true,
-			transformRequest : null
-		}).then(function(response) {
-				callback(false, response, response.data);
-			}, function(response) {
-				callback(true, response, response.data);
-			});
+		self.config.upload.url  = url;
+		self.config.upload.data = sendParams;
+
+		send(self.config.upload, function(error, httpResponse, body) {
+			if (callback) callback(error, httpResponse, body);
+		});
+	};
+
+	var send = function(config, callback) {
+		$http(config).then(
+			function(response) { if (callback) callback(false, response, response.data); },
+			function(response) { if (callback) callback(true, response, response.data); }
+		);
 	};
 
 	return self;
